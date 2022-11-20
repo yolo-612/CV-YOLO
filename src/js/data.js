27c86b716dc2701fc2269fa13687235524e8c4c7
keruisiya <2458097476@qq.com>
2022-11-20 18:54:09 +0800

    resume 1.0

--- src/js/data.js
+++ src/js/data.js
@@ -1,4 +1,5 @@
 import eTx from './../assets/logo_tx.png';
+import eSf from './../assets/logo_sf.png';
 import eJm from './../assets/logo_jm.png';
 import eLt from './../assets/logo_lt.png';
 import eZh from './../assets/logo_zh.png';
@@ -44,9 +45,9 @@ export default {
     },
     footer: ['Released under the MIT License', 'All Rights Reserved ', `Copyright © 2014-${new Date().getFullYear()} Korey Zhao`],
     overview: {
-      infoList: ['30岁', '本科', '成都', '在职'],
-      quote: '生活是一种绵延不绝的渴望，渴望不断上升，变得更伟大而高贵。',
-      desList: ['我叫赵柯宇，腾讯资深前端工程师', '曾聚美优品电商&增长团队前端负责人', 'keyu.zhao@foxmail.com']
+      infoList: [`${new Date().getFullYear() - 1996}岁`, '硕士', '深圳', '在职'],
+      quote: '善始者实繁,克终者盖寡',
+      desList: ['我叫戴金森，毕业于南京邮电大学', '现于顺丰科技/客户渠道产品研发中心担任前端开发工程师', 'keruisiya0818@163.com']
     },
     skill: {
       outCircleList: [
@@ -60,13 +61,13 @@ export default {
           bg: {
             backgroundColor: 'rgba(49,65,82,0.8)'
           },
-          name: 'react'
+          name: 'Html5'
         },
         {
           bg: {
             backgroundColor: 'rgba(76,157,160,0.8)'
           },
-          name: 'angular'
+          name: 'Css3'
         },
         {
           bg: {
@@ -84,13 +85,13 @@ export default {
           bg: {
             backgroundColor: 'rgba(117,148,179,0.8)'
           },
-          name: '快应用'
+          name: 'Js'
         },
         {
           bg: {
             backgroundColor: 'rgba(71,83,94,0.8)'
           },
-          name: 'ssr'
+          name: 'Git'
         },
         {
           bg: {
@@ -104,7 +105,7 @@ export default {
           bg: {
             backgroundColor: 'rgba(179,164,140,0.8)'
           },
-          name: '构建'
+          name: '调试'
         },
         {
           bg: {
@@ -122,127 +123,135 @@ export default {
           bg: {
             backgroundColor: 'rgba(207,184,178,0.8)'
           },
-          name: '部署'
+          name: '开发'
         }
+        // {
+        //   bg: {
+        //     backgroundColor: 'rgba(207,184,178,0.8)'
+        //   },
+        //   name: '部署'
+        // }
       ],
       desList: [
-        '七年前端开发经验，两年管理经验',
-        '实际主导开发过多平台项目（APP，PC，H5，小程序，快应用等），制定并规范团队协作模式',
-        '前端技术栈均有尝试，擅长组件开发及构建优化',
-        '自我驱动力强，写有webpack、vue等源码分析系列文章'
+        '组内担任【月结管家】--签约模块业务日常开发',
+        '涉及PC、H5、小程序、微应用多端开发',
+        '前端技术栈均有尝试，擅长组件开发及构建优化....',
+        '自我驱动力强，写有webpack、vue等源码分析系列文章....'
       ]
     },
     exp: {
       expList: [
         {
-          title: '腾讯',
-          time: '2021年4月~至今',
-          post: '资深前端工程师',
+          title: '顺丰科技',
+          time: '2021年5月~至今',
+          post: '前端开发工程师',
           img: {
-            backgroundImage: `url(${eTx})`
+            backgroundImage: `url(${eSf})`,
+            backgroundSize: '100%'
           },
-          contentList: ['负责腾讯优码项目的开发工作', '参与制定团队协作规范', '参与一些优码客户小程序、B端开发', '参与开源社区贡献']
-        },
-        {
-          title: '聚美优品',
-          time: '2017年4月~2021年4月',
-          post: '资深前端工程师/前端负责人',
-          img: {
-            backgroundImage: `url(${eJm})`
-          },
-          contentList: ['负责聚美优品电商及增长FE团队(10+)的日常工作', '参与街电CRM开发，美点无人货架C/S开发等', '主导开发聚美等若干小程序/快应用及增长活动', '优化重构聚美底层若干库如jumei-sdk等']
-        },
-        {
-          title: '兰途科技',
-          time: '2015年6月-2017年4月',
-          post: 'web前端研发工程师',
-          img: {
-            backgroundImage: `url(${eLt})`
-          },
-          contentList: ['负责高校校园移动APP相关的H5混合开发', '负责快速构建高质量移动APP/PC/微信页面', '根据业务需求设计合适的工程项目', '高度组件化、模块化思维，拥有各平台调试能力']
-        },
-        {
-          title: '卓煌企业',
-          time: '2014年8月~2015年6月',
-          post: '网页开发兼SEO优化',
-          img: {
-            backgroundImage: `url(${eZh})`
-          },
-          contentList: ['负责各企业站静态页开发及简单交互', '负责网站后台管理页面开发', '负责百度竞价操作及seo优化', '同期工作之余提升自己原生JS的编码能力']
+          contentList: ['负责月结管家项目的开发工作', '参与制定团队规范建设', '参与日常owner模块需求拆解、开发联调、代码评审', '']
         }
+        // {
+        //   title: '聚美优品',
+        //   time: '2017年4月~2021年4月',
+        //   post: '资深前端工程师/前端负责人',
+        //   img: {
+        //     backgroundImage: `url(${eJm})`
+        //   },
+        //   contentList: ['负责聚美优品电商及增长FE团队(10+)的日常工作', '参与街电CRM开发，美点无人货架C/S开发等', '主导开发聚美等若干小程序/快应用及增长活动', '优化重构聚美底层若干库如jumei-sdk等']
+        // }
+        // {
+        //   title: '兰途科技',
+        //   time: '2015年6月-2017年4月',
+        //   post: 'web前端研发工程师',
+        //   img: {
+        //     backgroundImage: `url(${eLt})`
+        //   },
+        //   contentList: ['负责高校校园移动APP相关的H5混合开发', '负责快速构建高质量移动APP/PC/微信页面', '根据业务需求设计合适的工程项目', '高度组件化、模块化思维，拥有各平台调试能力']
+        // },
+        // {
+        //   title: '卓煌企业',
+        //   time: '2014年8月~2015年6月',
+        //   post: '网页开发兼SEO优化',
+        //   img: {
+        //     backgroundImage: `url(${eZh})`
+        //   },
+        //   contentList: ['负责各企业站静态页开发及简单交互', '负责网站后台管理页面开发', '负责百度竞价操作及seo优化', '同期工作之余提升自己原生JS的编码能力']
+        // }
       ]
     },
     works: {
       worksList: [
         {
-          title: '聚美优品+ 小程序',
+          title: '月结管家公众号',
           des: '聚美优品+小程序是聚美集团电商业务在微信小程序上的实现，包括普通商品，拼团商品的销售流程及购物流程、用户中心等模块。',
           mini: wJm,
           style: {}
-        },
-        {
-          title: '鹿尚生活 小程序',
-          des: '鹿尚生活小程序是聚美集团专柜购业务在微信小程序上的实现，包括专柜购区域选择及提货方式等全套销售流程及购物流程、用户中心等模块。',
-          mini: wLs,
-          style: {}
-        },
-        {
-          title: '聚美优品 快应用',
-          des: '聚美优品快应用是聚美集团电商业务在安卓快应用上的实现，基于手机硬件平台的新型应用形态，为销售途径提供新的渠道。',
-          url: 'http://hapjs.org/app/com.jm.quickapp',
-          style: {}
-        },
-        {
-          title: '聚美优品 H5',
-          des: '聚美集团电商业务移动端的核心承载者，包括前三页，购物流程，用户中心等，通过jsbridge部分页面也在app内广泛应用。',
-          url: 'https://h5.jumei.com',
-          style: {}
-        },
-        {
-          title: '聚美优品 增长活动',
-          des: '聚美集团电商业务的新增长投放项目，电商主要的推广活动承载页面，实现采用多页面架构，支持打包单一活动的工作模式。',
-          url: 'http://r.jumei.com',
-          style: {}
-        },
-        {
-          title: '街电crm后台管理系统',
-          des: 'PC端街电crm后台商户管理系统，包括各种商户录入，统计，维护等若干功能。',
-          url: '',
-          style: {}
-        },
-        {
-          title: '移动校园APP',
-          des:
-            '一站式、全生命周期的校园官方移动服务平台，涵盖PC、安卓、IOS、微信平台，上线学校包括中央财经大学，北京交通大学，中国戏曲学院，辽宁大学，西南财经大学等等，下载量数十万，深受学生老师喜爱。',
-          url: 'http://app.cufe.edu.cn/',
-          style: {}
-        },
-        {
-          title: '移动校园管理系统',
-          des: '针对校园APP的后台管理系统，包括人员架构，信息统计，权限设置，H5生产机，数据交换平台，各个模块的数据管理，反馈设置等等，为高校提供基于互联网运营的移动校园整体解决方案。',
-          url: '',
-          style: {}
-        },
-        {
-          title: 'web前端工程师简历（2015年版）',
-          des: '本简历初版，上线一个月后，百度关键词“web前端工程师简历”排名前三，点击量数百万，深受广大前端初学者的好评及模仿，至今搜该关键词百度前10页都处处能见该简历的仿版。',
-          url: 'http://www.flqin.com/2015/',
-          style: {}
-        },
-        {
-          title: '@korey/MVVM',
-          des: '一个简易的MVVM框架，实现了data-binding 和 view-refresh的功能，目前仅服务于本简历项目，仍在不断优化更新，欢迎学习交流',
-          url: 'https://github.com/zhaoky/mvvm',
-          style: {}
-        },
-        {
-          title: '@korey/Fullpage',
-          des: '一个简易的Fullpage框架，实现了上拉下拉，滑轮滚动，锚点直达的全屏翻页功能，目前仅服务于本简历项目，仍在不断优化更新，欢迎学习交流',
-          url: 'https://github.com/zhaoky/fullpage',
-          style: {}
         }
+        // {
+        //   title: '鹿尚生活 小程序',
+        //   des: '鹿尚生活小程序是聚美集团专柜购业务在微信小程序上的实现，包括专柜购区域选择及提货方式等全套销售流程及购物流程、用户中心等模块。',
+        //   mini: wLs,
+        //   style: {}
+        // },
+        // {
+        //   title: '聚美优品 快应用',
+        //   des: '聚美优品快应用是聚美集团电商业务在安卓快应用上的实现，基于手机硬件平台的新型应用形态，为销售途径提供新的渠道。',
+        //   url: 'http://hapjs.org/app/com.jm.quickapp',
+        //   style: {}
+        // },
+        // {
+        //   title: '聚美优品 H5',
+        //   des: '聚美集团电商业务移动端的核心承载者，包括前三页，购物流程，用户中心等，通过jsbridge部分页面也在app内广泛应用。',
+        //   url: 'https://h5.jumei.com',
+        //   style: {}
+        // },
+        // {
+        //   title: '聚美优品 增长活动',
+        //   des: '聚美集团电商业务的新增长投放项目，电商主要的推广活动承载页面，实现采用多页面架构，支持打包单一活动的工作模式。',
+        //   url: 'http://r.jumei.com',
+        //   style: {}
+        // },
+        // {
+        //   title: '街电crm后台管理系统',
+        //   des: 'PC端街电crm后台商户管理系统，包括各种商户录入，统计，维护等若干功能。',
+        //   url: '',
+        //   style: {}
+        // },
+        // {
+        //   title: '移动校园APP',
+        //   des:
+        //     '一站式、全生命周期的校园官方移动服务平台，涵盖PC、安卓、IOS、微信平台，上线学校包括中央财经大学，北京交通大学，中国戏曲学院，辽宁大学，西南财经大学等等，下载量数十万，深受学生老师喜爱。',
+        //   url: 'http://app.cufe.edu.cn/',
+        //   style: {}
+        // },
+        // {
+        //   title: '移动校园管理系统',
+        //   des: '针对校园APP的后台管理系统，包括人员架构，信息统计，权限设置，H5生产机，数据交换平台，各个模块的数据管理，反馈设置等等，为高校提供基于互联网运营的移动校园整体解决方案。',
+        //   url: '',
+        //   style: {}
+        // },
+        // {
+        //   title: 'web前端工程师简历（2015年版）',
+        //   des: '本简历初版，上线一个月后，百度关键词“web前端工程师简历”排名前三，点击量数百万，深受广大前端初学者的好评及模仿，至今搜该关键词百度前10页都处处能见该简历的仿版。',
+        //   url: 'http://www.flqin.com/2015/',
+        //   style: {}
+        // },
+        // {
+        //   title: '@korey/MVVM',
+        //   des: '一个简易的MVVM框架，实现了data-binding 和 view-refresh的功能，目前仅服务于本简历项目，仍在不断优化更新，欢迎学习交流',
+        //   url: 'https://github.com/zhaoky/mvvm',
+        //   style: {}
+        // },
+        // {
+        //   title: '@korey/Fullpage',
+        //   des: '一个简易的Fullpage框架，实现了上拉下拉，滑轮滚动，锚点直达的全屏翻页功能，目前仅服务于本简历项目，仍在不断优化更新，欢迎学习交流',
+        //   url: 'https://github.com/zhaoky/fullpage',
+        //   style: {}
+        // }
       ],
-      viewMore: 'github上面查看更多'
+      viewMore: 'github上面查看更多',
+      gitUrl: 'https://github.com/yolo-612'
     },
     contact: {
       title: '联系我',
@@ -262,24 +271,24 @@ export default {
       typeList: [
         {
           icon: sGithub,
-          url: 'https://github.com/zhaoky'
+          url: 'https://github.com/yolo-612'
         },
         {
           icon: sSf,
-          url: 'https://segmentfault.com/u/fe_korey'
+          url: ''
         },
         {
           icon: sBlog,
-          url: 'https://blog.flqin.com'
-        },
-        {
-          icon: sZh,
-          url: 'https://www.zhihu.com/people/fe_korey'
-        },
-        {
-          icon: sJj,
-          url: 'https://juejin.im/user/57c91b3b165abd0068db89c2'
+          url: ''
         }
+        // {
+        //   icon: sZh,
+        //   url: 'https://www.zhihu.com/people/fe_korey'
+        // },
+        // {
+        //   icon: sJj,
+        //   url: 'https://juejin.im/user/57c91b3b165abd0068db89c2'
+        // }
       ]
     }
   },
